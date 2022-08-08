5982f0157b9700a5019646084324663855b9a5e3
mazoureb <mazourek420@gmail.com>
2022-08-08 19:16:46 +0200

    Room table addition

--- Hotel-Database/2-fill_database.sql
+++ Hotel-Database/2-fill_database.sql
@@ -1,10 +1,11 @@
-use Hotel;
+use HotelDatabase;
+
+insert into Hotel values ('Creaky Cabin', 'Legion Rd 24', '47006', 'Ballstown, Indiana', 'USA', 2);
 
 insert into JobType values('Reception', 2500);
 insert into JobType values('Janitor', 1800);
 insert into JobType values('Room Service', 2200);
 insert into JobType values('Bartender', 2700);
 
-select * from JobType ;
+insert into Employee values (1, 1, 'Johnny', 'Mark', 'M', '2008-11-11')
 
-insert into Hotel values ('Vagabond Cabin', 'Legion Rd 24', '47006', 'Ballstown, Indiana', 'USA', 2);
---
+++ Hotel-Database/5-delete_schema.sql
@@ -0,0 +1,10 @@
+use HotelDatabase
+
+drop table Employee;
+drop table JobType;
+drop table Hotel;
+
+drop table RoomType;
+drop table Room;
+
+
