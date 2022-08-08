e84b666765c5df84623f8fcbd4390b7db64f207a
mazoureb <mazourek420@gmail.com>
2022-08-08 18:57:28 +0200

    Database Filling

---
+++ Hotel-Database/2-fill_database.sql
@@ -0,0 +1,10 @@
+use Hotel;
+
+insert into JobType values('Reception', 2500);
+insert into JobType values('Janitor', 1800);
+insert into JobType values('Room Service', 2200);
+insert into JobType values('Bartender', 2700);
+
+select * from JobType ;
+
+insert into Hotel values ('Vagabond Cabin', 'Legion Rd 24', '47006', 'Ballstown, Indiana', 'USA', 2);
