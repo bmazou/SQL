97740cdcfbe141e678fcca171bda0f5c6fa4c63f
mazoureb <mazourek420@gmail.com>
2022-08-09 11:57:20 +0200

    guest,reservation,bill tables

--- Hotel-Database/1-tables.sql
+++ Hotel-Database/1-tables.sql
@@ -36,11 +36,14 @@ create table Employee (
   JobTypeID int not null
     constraint Employee_FK_JobType references JobType(JobTypeID)
       on delete cascade,
-  Name nvarchar(50) not null,
+  FirstName nvarchar(50) not null,
+  LastName nvarchar(50) not null,
   Gender char(1) not null
-    constraint Employee_MFO check (Gender in ('M','F','O')), 
+    constraint Employee_CHK_Gender 
+      check (Gender in ('M','F','O')), 
   Started date not null
 );
+
 
 select * from Employee;
 drop table Employee;
@@ -55,3 +58,69 @@ create table RoomType (
 
 select * from RoomType;
 
+
+--TODO Můžu dát trigger ať to nahlásí systému, pokud se smaže jeho RoomType + ať se ujistí že Room má svůj RoomType
+create table Room (
+  RoomID int identity(1,1)
+    constraint Room_PK primary key,
+  RoomTypeID int
+    constraint Room_FK_RoomType references RoomType(RoomTypeID)
+      on delete set null,
+  HotelID int not null
+    constraint Room_FK_Hotel references Hotel(HotelID)
+      on delete cascade,
+);
+
+select * from Room;
+
+
+
+create table Guest (
+  GuestID int identity(1,1)
+    constraint Guest_PK primary key,
+  FirstName nvarchar(50) not null,
+  LastName nvarchar(50) not null,
+  Email nvarchar(50)
+    constraint Guest_CHK_Email
+      check (Email like '_%@%_.%_'), 
+  Phone char(9)
+    constraint Guest_CHK_Phone 
+      check (Phone like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+  constraint Guest_NN_EmailPhone 
+    check (Email is not null or Phone is not null)
+);
+drop table Reservation;
+drop table Guest;
+select * from Guest;
+
+create table Reservation (
+  ReservationID int identity(1,1)
+    constraint Reservation_PK primary key,
+  RoomID int not null
+    constraint Reservation_FK_Room references Room(RoomID)
+      on delete cascade,
+  GuestID int not null
+    constraint Reservation_FK_Guest references Guest(GuestID)
+     on delete cascade,
+  StartDate date not null,
+  EndDate date not null,
+  NumOfGuests smallint not null,
+  constraint Reservation_CHK_StartBeforeEnd
+    check (StartDate < EndDate)
+);
+
+select * from Reservation;
+
+create table Bill (
+  BillID int identity(1,1)
+    constraint Bill_PK primary key,
+  ReservationID int not null
+    constraint Bill_FK_Reservation references Reservation(ReservationID)
+      on delete cascade,
+  --TODO Automaticky vytvořit ten roomcharge 
+  RoomCharge decimal(11,2) not null,
+  PaymentType char(4) not null
+    constraint Bill_CHK_PaymentType
+      check (PaymentType in ('Cash', 'Card')),
+  PaymentDate datetime
+)
--- Hotel-Database/2-fill_database.sql
+++ Hotel-Database/2-fill_database.sql
@@ -9,3 +9,11 @@ insert into JobType values('Bartender', 2700);
 
 insert into Employee values (1, 1, 'Johnny', 'Mark', 'M', '2008-11-11')
 
+insert into RoomType values (3, 49.9)
+
+insert into Room values (1,1)
+
+insert into Guest values ('Jasmine', 'Maurice', 'JM@gmail.com', null)
+insert into Guest values ('Jana', 'Ratu', null, '908312322')
+
+insert into Reservation values (1,1, '2022-02-02', '2022-05-02', 3)
